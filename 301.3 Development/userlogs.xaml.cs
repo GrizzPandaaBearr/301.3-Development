ff2d0eab0b0e10a80c32310047c90a1ab33c62e2
GrizzPandaaBearr <142283652+GrizzPandaaBearr@users.noreply.github.com>
2025-10-16 09:12:13 +1300

    changes

---
+++ 301.3 Development/userlogs.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _301._3_Development
+{
+    public partial class userlogs : Page
+    {
+        private ObservableCollection<UserLog> allLogs;
+
+        public userlogs()
+        {
+            InitializeComponent();
+            LoadUserLogs();
+
+            SearchBox.TextChanged += SearchBox_TextChanged;
+
+            FinishButton.Click += FinishButton_Click;
+        }
+
+        private void LoadUserLogs()
+        {
+            allLogs = new ObservableCollection<UserLog>
+            {
+
+            };
+
+            LogsDataGrid.ItemsSource = allLogs;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = SearchBox.Text.ToLower();
+
+            if (string.IsNullOrWhiteSpace(searchText) || searchText == "search by name")
+            {
+                LogsDataGrid.ItemsSource = allLogs;
+                return;
+            }
+
+            var filteredLogs = allLogs
+                .Where(log => log.User.ToLower().Contains(searchText))
+                .ToList();
+
+            LogsDataGrid.ItemsSource = filteredLogs;
+        }
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new adminscreen());
+        }
+    }
+    public class UserLog
+    {
+        public required string User { get; set; }
+        public required string Date { get; set; }
+        public required string Time { get; set; }
+        public required string Action { get; set; }
+        public required string LastEdited { get; set; }
+    }
+}
